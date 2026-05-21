db09bef120b15edf1d70c2fb4089aef5de4cce6a
Devansh Shukla <shukladevansh101@gmail.com>
2026-05-21 21:43:00 +0530

    some ques

--- src/LL/questions1.java
+++ src/LL/questions1.java
@@ -1,30 +1,80 @@
-//package LL;
-////leetcode 2 - add numbers
-//public class questions1 {
-//        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//            ListNode dummy = new ListNode();
-//            ListNode temp=dummy;
-//            int carry=0;
-//
-//            while(l1 !=null || l2 !=null || carry!=0){
-//                int sum = carry;
-//
-//                if(l1 !=null){
-//                    sum += l1.val;
-//                    l1=l1.next;
-//                }
-//                if(l2 !=null){
-//                    sum += l2.val;
-//                    l2=l2.next;
-//                }
-//                //dummy->0
-//                temp.next=new ListNode(sum%10);
-//                carry = sum / 10;
-//
-//                temp = temp.next; //dummy->0->temp.next
-//            }
-//
-//            return dummy.next;
-//        }
-//    }
-//
+package LL;
+public class questions1 {
+    // ListNode class
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    // add two numbers
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            // create new node
+            temp.next = new ListNode(sum % 10);
+
+            // update carry
+            carry = sum / 10;
+
+            // move temp
+            temp = temp.next;
+        }
+
+        return dummy.next;
+    }
+
+    // display linked list
+    public static void display(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+
+        System.out.println("END");
+    }
+
+    // main method
+    public static void main(String[] args) {
+
+        questions1 obj = new questions1();
+
+        // first number: 342
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        // second number: 465
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = obj.addTwoNumbers(l1, l2);
+
+        System.out.println("Result:");
+        display(result);
+    }
+}
