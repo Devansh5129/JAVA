d5e79f68d0afd55753138c5ccbcb1fc607df5f15
Devansh Shukla <shukladevansh101@gmail.com>
2026-05-22 15:21:46 +0530

    stockSpan

--- src/impQ/StockSpan.java
+++ src/impQ/StockSpan.java
@@ -1,30 +1,32 @@
 package impQ;
-//find : prevHIgh=> span - i - prevHIgh
 import java.util.*;
-
-class StockSpan{
-    Stack<int[]> stack;
-    public StockSpan() {
-        stack = new Stack<>();
-    }
-    public int next(int price) {
-        int span = 1;
-        // remove smaller/equal prices
-        while (!stack.isEmpty() && stack.peek()[0] <= price) {
-            span += stack.pop()[1];
-        }
-        // store {price, span}
-        stack.push(new int[]{price, span});
-        return span;
-    }
-
-        public static void main(String[] args) {
-            StockSpan ss = new StockSpan();
-            int[] prices = {100, 80, 60, 70, 60, 75, 85};
-
-            for (int price : prices) {
-                System.out.println("Price: " + price +
-                        " -> Span: " + ss.next(price));
+public class StockSpan {
+    public static void main(String[] args) {
+        // stock prices
+        int[] price = {100, 80, 60, 70, 60, 75, 85};
+        // answer array
+        int[] ans = new int[price.length];
+        // stack stores indices
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < price.length; i++) {
+            // remove smaller or equal elements
+            while (!stack.isEmpty() && price[stack.peek()] <= price[i]) {
+                stack.pop();
             }
+            // if no greater element on left
+            if (stack.isEmpty()) {
+                ans[i] = i + 1;
+            }
+            else {
+                // distance from previous greater element
+                ans[i] = i - stack.peek();
+            }
+            // push current index
+            stack.push(i);
         }
+        // print answer
+        for (int val : ans) {
+            System.out.print(val + " ");
+        }
+    }
 }
