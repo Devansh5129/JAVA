3280dd281d05ff323d6f201a89315c5a23086958
Devansh Shukla <shukladevansh101@gmail.com>
2026-05-29 09:50:09 +0530

    armstrong added

--- src/amstrong.java
+++ src/amstrong.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
 public class amstrong {
-   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-       System.out.println(" Enter a number : ");
-    int n = sc.nextInt();
-       int r;
-       int temp=n;
-       int sum=0;
-       while(n>0){
-           r =n%10;
-           n = n/10;
-            sum = sum + r*r*r;
-       }
-       if(sum==temp){
+    public static void main(String[] args) {
 
-           System.out.println("it is an armstrong number");
-       }
-       else {
-           System.out.println("not ");
-       }
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int temp = n;
+        int sum = 0;
+        int r;
+
+        while (n > 0) {
+            r = n % 10;
+            sum = sum + (r * r * r);
+            n = n / 10;
+        }
+
+        if (sum == temp) {
+            System.out.println("It is an Armstrong number");
+        } else {
+            System.out.println("Not an Armstrong number");
+        }
+
+        sc.close();
     }
 }
