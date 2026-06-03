21f65a5d585b33d919c49d60641b7f07a24af237
Devansh Shukla <shukladevansh101@gmail.com>
2026-06-03 18:58:51 +0530

    maxCards leet 1423 added

---
+++ src/maxcardsPoint.java
@@ -0,0 +1,29 @@
+class maxcardsPoint {
+    static void main(String[] args) {
+        int [] cardPoints ={6,2,3,4,7,2,1,7,1}; int k =4;
+        System.out.println(maxScore(cardPoints,k));
+    }
+    public static int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int lsum = 0;
+        int rsum = 0;
+        int maxsum = 0;
+
+        // left se cards le lenge pahle 
+        for(int i = 0; i < k; i++) {
+            lsum += cardPoints[i];
+        }
+        maxsum = lsum;
+        int rightIndex = n - 1;
+        // Remove one from left and add one from right
+        for(int i = k - 1; i >= 0; i--) {
+
+            lsum -= cardPoints[i];
+            rsum += cardPoints[rightIndex];
+            // right ko badhane ke liye right -- karte jao...
+            rightIndex--;
+            maxsum = Math.max(maxsum, lsum + rsum);
+        }
+        return maxsum;
+    }
+}
