5515d737e84303a2d05b4added95602a18d6d56f
Devansh Shukla <shukladevansh101@gmail.com>
2026-06-05 23:07:09 +0530

    max consecutive ones(brute and optimal) added

---
+++ src/impQ/maxConseCutiveOnes.java
@@ -0,0 +1,22 @@
+package impQ;
+class maxConseCutiveOnes {
+    static void main(String[] args) {
+
+    }
+    public int longestOnes(int[] nums, int k) {
+        int maxLen = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int zeros = 0;
+            for(int j = i; j < nums.length; j++) {
+                if(nums[j] == 0) {
+                    zeros++;
+                }
+                if(zeros > k) {
+                    break;
+                }
+                maxLen = Math.max(maxLen, j - i + 1);
+            }
+        }
+        return maxLen;
+    }
+}
