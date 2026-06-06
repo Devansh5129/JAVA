7aaa7eecfbdc4090eb21dc8f6027bc12a9e22781
Devansh Shukla <shukladevansh101@gmail.com>
2026-06-06 10:05:25 +0530

    max consecutive onesIII(brute and optimal) added

--- src/impQ/maxConseCutiveOnes.java
+++ src/impQ/maxConseCutiveOnes.java
@@ -1,9 +1,11 @@
-package impQ;
+//brute and optimal approaches
 class maxConseCutiveOnes {
     static void main(String[] args) {
-
+        int [] nums={1,1,1,0,0,0,1,1,1,1,0};
+        System.out.println(longestOnes1(nums,2));
+        System.out.println(longestOnes(nums,2));
     }
-    public int longestOnes(int[] nums, int k) {
+    public static int longestOnes(int[] nums, int k){
         int maxLen = 0;
         for(int i = 0; i < nums.length; i++) {
             int zeros = 0;
@@ -19,4 +21,22 @@ class maxConseCutiveOnes {
         }
         return maxLen;
     }
-}
+        public static int longestOnes1(int[] nums, int k) {
+            int left = 0;
+            int zeros = 0;
+            int maxLen = 0;
+            for(int right = 0; right < nums.length; right++) {
+                if(nums[right] == 0) {
+                    zeros++;
+                }
+                while(zeros > k) {
+                    if(nums[left] == 0) {
+                        zeros--;
+                    }
+                    left++;
+                }
+                maxLen = Math.max(maxLen, right-left + 1);
+            }
+            return maxLen;
+        }
+    }
